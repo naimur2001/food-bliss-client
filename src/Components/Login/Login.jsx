829f94a6207dfcf9945a4c560672984adf46d5d0
naimur2001 <naimurrahman18thapril@gmail.com>
2023-05-05 20:42:57 +0600

    login page updated

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -109,7 +109,7 @@ const handleGithub=(event)=>{
           Login with Github</button>
         <label className="label">
           Don't have an account --
-          <Link to='/register' className="label-text-alt link link-hover text-base text-red-400 mx-3">Regsiter</Link>
+          <Link to='/register' className="label-text-alt link link-hover text-base text-red-400 mx-3">register</Link>
         </label>
       </div>
     </div>
